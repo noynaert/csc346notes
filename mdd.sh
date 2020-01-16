5761bed0efe39355e6ad7f9dca5f22083dc90abe
J. Evan Noynaert <noynaert@missouriwestern.edu>
2020-01-16 14:36:26 -0600

    allow mdd.sh to pull data from a RAW md website

--- mdd.sh
+++ mdd.sh
@@ -1,11 +1,13 @@
 #!/bin/bash
 if [ $# -ne 1 ]; then
-  echo "Usage: $0 file.html"
+  echo "Usage: $0 file.html | url"
   exit 1
 fi
-if [ ! -f $1 ]; then
-  echo "File \"$1\" does not exist.  ***Aborting***"
-  exit 2
-else
+if [ -f $1 ]; then
   pandoc $1 | w3m -T text/html
+elif grep "https:" <<< "$1" ; then
+  curl -s $1 | pandoc | w3m -T text/html
+else
+  echo "File or URL \"$1\" does not exist.  ***Aborting***"
+  exit 2
 fi
